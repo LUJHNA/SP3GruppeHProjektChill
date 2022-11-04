283cd78efc4d43ac5963430246c49c694869baad
DimitraSis <dimitra.sis7@gmail.com>
2022-11-04 11:39:04 +0100

    abstract Media class

---
+++ AMedia.java
@@ -0,0 +1,14 @@
+public abstract class AMedia {
+
+    public int rating;
+
+    public int releaseYear;
+
+    public String category;
+
+    public AMedia(int rating, int releaseYear, String category) {
+        this.rating = rating;
+        this.releaseYear = releaseYear;
+        this.category = category;
+    }
+}
